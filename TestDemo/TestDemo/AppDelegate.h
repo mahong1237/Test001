563fc254f5b030a7cbc94502933400d430672a46
mahong1237 <shali1998@126.com>
2016-08-09 16:34:13 +0800

    A

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by lee on 16/8/9.
+//  Copyright © 2016年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by lee on 16/8/9.
+//  Copyright © 2016年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
